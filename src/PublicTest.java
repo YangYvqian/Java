63e9019c61a2ef8048c4a793efd2c8d3ff88465a
yyq <756304078@qq.com>
2021-10-31 10:48:56 +0800

    Java

---
+++ src/PublicTest.java
@@ -0,0 +1,13 @@
+public class PublicTest {
+    
+    public static void main(String[] args) {
+        String a = "123";
+        String b = "123";
+
+        if(a==b){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+}
